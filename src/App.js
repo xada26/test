a59c5c097e1e54e976be00f4b9cc6b73d187e7e5
Arya Dwi Aditiya <60702358+aryaada@users.noreply.github.com>
2021-12-09 13:10:52 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
                         return (
                             <tr key={i}>
                                 <td>
-                                    {ss1[1]}{ss2[1]}{console.log(totalll1)}
+                                    {ss1[1]}{ss2[1]}
                                 </td>
                                 <td>
                                     {ss2[3]}{ss1[3]}
